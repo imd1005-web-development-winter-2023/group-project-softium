19825fb191fc00c7ae5c4623fc0d76ad5ea51cb0
cynthia wang <cynthia.wang218@gmail.com>
2023-04-10 18:36:37 -0400

    changed datastructures.js

--- datastructures.js
+++ datastructures.js
@@ -1,6 +1,10 @@
 
 
 
+//button was clicked! //added this
+  document.querySelector('#button-1').addEventListener('click', function(){ console.log('The button was clicked!') });  
+  document.querySelector('#button-2').addEventListener('click', function(){ console.log('The button was clicked!') });
+
 //loop to draw through the buttons
 //first page, first option
 const page={
@@ -16,25 +20,19 @@ const page={
       //next image/ page, the 4th page
       load: 3,
     }
-  ]
+  ],
+  //added this
+  textContent: "sayHiHi",
 }
 
 const novel=[
   //first page inside datastructure, create a render to run the first page
   {
     imagePath: "./images/1.jpg", //background
-    options: [
-      {
-        text: "say Hi",
-        //next image/ page, the 4th page
-        load: 4,
-      },
-      {
-        text: "say Hi2",
-        //next image/ page, the 4th page
-        load: 3,
-      }
-    ]
+    //changed options out
+    options: [],
+    textContent: "",
+    load: 2,
   },
 
   //index 1
@@ -82,8 +80,10 @@ const currentPage= 0;
 function drawNovel() {
   if (novel[currentPage].options.length === 0)
   {
-    console.log("no Options")
+    console.log("no Options") //added 1 to current pages, added this
+    currentPage++;
   }
+
 
   for (let i= 0; i < novel[currentPage].options.length; i++ ){
     console.log("Option", novel[currentPage].options[i].text) //get button text
