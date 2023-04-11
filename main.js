f0949b9286344fde69a8e413c5e9eeaba6be1a1e
Cyrus Cheung <cyruscheung@cmail.carleton.ca>
2023-04-11 13:54:14 -0400

    added dialogue

--- main.js
+++ main.js
@@ -38,6 +38,42 @@ function draw() {
             nameT = them;
             say = "Who dares disturb me at this hour!?";
             break;
+        case 4:
+            nameT = you;
+            say = "...? Uh... hello, sir, I'm here to deliver your large vegetarian pi-";
+            break;
+        case 5:
+            nameT = them;
+            say = "Delightful! That was quick!";
+            break;
+        case 6:
+            nameT = you;
+            say = "Uh... really? The drive over actually took quite a while...";
+            break;
+        case 7:
+            nameT = them;
+            say = "Ah, of course. Forgive me; time passes quickly when you're immortal.";
+            break;
+        case 8:
+            nameT = you;
+            say = "A-Are you a... vampire..??";
+            break;
+        case 9:
+            nameT = them;
+            say = "Yes. Why, is that a problem?";
+            break;
+        case 10:
+            nameT = you;
+            say = "N-No! No! It's just.. I never would've thought that vampires would eat.. Vegetarian pizzas?";
+            break;
+        case 11:
+            nameT = them;
+            say = "Oh goodness no. I always order the meat lovers' flavour. After all, someone with a lifestyle such as mine must carefully watch their physique! I just thought I'd try something different for once. Eternal life becomes kind of... boring after a while, wouldn't you agree?";
+            break;
+        case 12:
+            nameT = you;
+            say = "A-Ah... yes. I see...";
+            break;
     }
     name_text.textContent = nameT;
     say_text.textContent = say;
